6337d075ca5b98b69091a57d6b327ea634e04f90
sp69ke <2351587229@qq.com>
2023-12-30 18:32:25 +0800

    add: class Layout

---
+++ layout.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Layout:
+    def __init__(self):
+        self.tiles = []
+        self.font = pygame.font.Font(None, 50)
+        self.tile_size = 100
+        self.margin = 10
+
+        for i in range(3):
+            for j in range(3):
+                tile = NumberTile(i * 3 + j, j * (self.tile_size + self.margin) + self.margin,
+                                  i * (self.tile_size + self.margin) + self.margin)
+                self.tiles.append(tile)
+
+    def draw(self, surface):
+        for tile in self.tiles:
+            tile.draw(surface, self.font)
