f41dba5413ef568d9aa33e06c6ce6ca83a424d19
sp69ke <2351587229@qq.com>
2023-12-30 18:35:47 +0800

    add: class NumberTile

---
+++ board.py
@@ -0,0 +1,12 @@
+class NumberTile:
+    def __init__(self, value, x, y):
+        self.value = value
+        self.x = x
+        self.y = y
+
+    def draw(self, surface, font):
+        if self.value != 0:
+            text = font.render(str(self.value), True, (255, 255, 255))
+            text_rect = text.get_rect()
+            text.center = (self.x + 50, self.y + 50)
+            surface.blit(text, text_rect)
