f9f133071ce1b5e5ef1a2a63888e6cadcc58a36e
sp69ke <2351587229@qq.com>
2023-12-30 18:10:55 +0800

    commit all

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-import sys
 import pygame
+import sys
 from pygame.locals import *
 
-blocks = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8}
 
-
-def drawTable(screen):
-    pygame.draw.rect(screen, Color("black"), (50, 50, 500, 500), 1)
-
-
-def startGame():
-    # init
-    pygame.init()
-    size = width, height = 800, 600
-    bg_color = (255, 255, 255)
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("八数码游戏")
-
-    # main loop
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        screen.fill(bg_color)
-        drawTable(screen)
-        pygame.display.update()
-
-
-if __name__ == "__main__":
-    startGame()
+# main loop
