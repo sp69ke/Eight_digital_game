b3509135419aa6c980595bd43587892bd8ee6792
sp69ke <2351587229@qq.com>
2023-12-30 13:47:40 +0800

    import pygame and create a window.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+import sys
+
+
+def init():
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("八数码游戏")
+
+
+def startGame():
+    init()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    startGame()
